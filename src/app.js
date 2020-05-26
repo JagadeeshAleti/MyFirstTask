6f0b0cadde86087dd9cf7433362634bf2a5da21f
JagadeeshAleti <jagadeesh.aleti955@gmail.com>
2020-05-26 13:34:05 +0530

    third commit

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const path = require('path')
 const hbs = require('hbs')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Defines path for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -29,6 +30,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000')
+app.listen(port, () => {
+    console.log('Server running on port ' + port)
 })
